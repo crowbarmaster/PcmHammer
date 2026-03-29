ecf3edcf6a65608788d04e0590b3329310d5a0b7
Crowbarmaster <Crowbarmasterhl2@gmail.com>
2026-03-28 23:51:51 -0400

    This class handles Bluetooth device discovery, only targeting paired devices.
    It should be noted that device descriptors are set here to avoid pooling unusable devices.

---
+++ Apps/PcmLibrary/Ports/SerialBluetoothDiscovery.cs
@@ -0,0 +1,38 @@
+﻿using InTheHand.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PcmHacking
+{
+    public class BluetoothPortConfiguration : PortConfiguration
+    {
+        public BluetoothDeviceInfo DeviceInfo;
+
+        public BluetoothPortConfiguration(BluetoothDeviceInfo bluetoothDevice) { 
+            DeviceInfo = bluetoothDevice;
+        }
+    }
+
+    public static class SerialBluetoothDiscovery
+    {
+        private static readonly List<string> _allowedPrefixes = ["OBDX", "OBDLink"];
+
+        public static IEnumerable<BluetoothDeviceInfo> GatherPairedDevices()
+        {
+            List<BluetoothDeviceInfo> infos = [];
+            BluetoothClient bluetoothClient = new();
+
+            foreach (BluetoothDeviceInfo device in bluetoothClient.PairedDevices)
+            {
+                _allowedPrefixes.ForEach(p =>
+                {
+                    if (device.DeviceName.StartsWith(p))
+                    {
+                        infos.Add(device);
+                    }
+                });
+            }
+            return infos;
+        }
+    }
+}
